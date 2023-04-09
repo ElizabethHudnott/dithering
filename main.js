4cddbd9d60f77b48a92396d0b9a9a7fbd214804b
Elizabeth Hudnott <ElizabethHudnott@users.noreply.github.com>
2023-04-09 02:22:33 +0100

    Upscale using CSS.

--- main.js
+++ main.js
@@ -12,12 +12,29 @@ function hslaToRGBA(h, s, l, alpha) {
 	return [f(0), f(8), f(4), alpha];
 }
 
-function twelveBitColor(h, s, l) {
-	let [r, g, b] = hslaToRGBA(h / 90, s / 15, l / 30, 1);
+/**
+ * @param {number} h Hue between 0 and 89.
+ * @param {number} s Saturation between 0 and 15.
+ * @param {number} l Lightness between 0 and 30.
+ * @param {number} a Alpha, 0 or 1
+ */
+function twelveBitColor(h, s, l, a = 1) {
+	let [r, g, b] = hslaToRGBA(h * 4, s / 15, l / 30, 1);
 	r = Math.round(Math.round(r * 15) * 255 / 15);
 	g = Math.round(Math.round(g * 15) * 255 / 15);
 	b = Math.round(Math.round(b * 15) * 255 / 15);
-	return [r, g, b, 255];
+	return [r, g, b, a * 255];
+}
+
+function meanColor(color1, color2) {
+	const r = Math.trunc(0.5 * (color1[0] + color2[0]));
+	const g = Math.trunc(0.5 * (color1[1] + color2[1]));
+	const b = Math.trunc(0.5 * (color1[2] + color2[2]));
+	return `rgb(${r}, ${g}, ${b})`;
+}
+
+function colorString(color) {
+	return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
 }
 
 class DitherPattern {
@@ -47,53 +64,71 @@ class DitherPattern {
 	/**
 	 * @param {number[]} color
 	 */
-	async createPattern(context, color, scaleX, scaleY = scaleX) {
+	async createPattern(context, color) {
 		const width = this.width;
 		const height = this.height;
-		const imageData = context.createImageData(width * scaleX, height * scaleY);
-		const rowLength = 4 * width * scaleX;
+		const imageData = context.createImageData(width, height);
 		const pixels = imageData.data;
 		let offset = 0;
-		let copyOffset;
 		for (let j = 0; j < height; j++) {
 			const row = this.pattern[j];
-			const rowOffset = offset;
 			for (let i = 0; i < width; i++) {
 				if (row[i] === '1') {
 					pixels.set(color, offset);
-					copyOffset = offset + 4;
-					for (let k = 1; k < scaleX; k++) {
-						pixels.copyWithin(copyOffset, offset, offset + 4);
-						copyOffset += 4;
-					}
 				}
-				offset += 4 * scaleX;
+				offset += 4;
 			}
-			copyOffset = rowOffset + rowLength;
-			for (let k = 1; k < scaleY; k++) {
-				pixels.copyWithin(copyOffset, rowOffset, rowOffset + rowLength);
-				copyOffset += rowLength;
-			}
-			offset = copyOffset;
 		}
 		const bitmap = await createImageBitmap(imageData);
 		return context.createPattern(bitmap, 'repeat');
 	}
 
-	static checkerboard(modX = 2, modY = 1) {
+	static checkerboard(modX = 2, emptyRows = 0, right = false) {
+		const numRows = 2 + 2 * emptyRows;
+		const rows = new Array(numRows);
+		rows[0] = '1' + '0'.repeat(modX - 1);
+		const emptyRow = '0'.repeat(modX);
+		rows.fill(emptyRow, 1, 1 + emptyRows);
+		const rounding = right ? Math.ceil : Math.trunc;
+		const index = rounding(modX / 2);
+		rows[1 + emptyRows] = rows[0].slice(-index) + rows[0].slice(0, -index);
+		rows.fill(emptyRow, 2 + emptyRows, numRows)
+		return new DitherPattern(rows);
+	}
 
+	static verticalStripes(width = 1, gap = 1) {
+		const row = '1'.repeat(width) + '0'.repeat(gap);
+		return new DitherPattern([row]);
 	}
 }
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('pixel-canvas');
 const context = canvas.getContext('2d');
 
-let dither = DitherPattern.fromString('10|01');
-let pattern = await dither.createPattern(context, [0, 0, 0, 255], 4);
+let fgColor = twelveBitColor(30, 5, 5);
+let bgColor = twelveBitColor(30, 15, 5);
+let fgColorStr = colorString(fgColor);
+let bgColorStr = colorString(bgColor);
+let meanColorStr = meanColor(fgColor, bgColor);
+
+let dither = DitherPattern.verticalStripes();
+let pattern = await dither.createPattern(context, fgColor);
 
 function draw() {
+	const totalWidth = canvas.width;
+	const totalHeight = canvas.height;
+	const leftWidth = Math.ceil(0.5 * totalWidth);
+	const topHeight = Math.ceil(0.5 * totalHeight);
+	const rightWidth = totalWidth - leftWidth;
+	const bottomHeight = totalHeight - topHeight;
+	context.fillStyle = bgColorStr;
+	context.fillRect(0, 0, totalWidth, topHeight);
 	context.fillStyle = pattern;
-	context.fillRect(0, 0, canvas.width, canvas.height);
+	context.fillRect(0, 0, leftWidth, topHeight);
+	context.fillStyle = meanColorStr;
+	context.fillRect(0, topHeight, leftWidth, bottomHeight);
+	context.fillStyle = fgColorStr;
+	context.fillRect(leftWidth, topHeight, rightWidth, bottomHeight);
 }
 
 draw();
