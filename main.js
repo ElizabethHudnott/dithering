2f647a9e98145acbcf085d593bd27d01a9a05bd4
Elizabeth Hudnott <ElizabethHudnott@users.noreply.github.com>
2023-04-09 16:12:27 +0100

    Change from strings to numbers.

--- main.js
+++ main.js
@@ -18,12 +18,20 @@ function hslaToRGBA(h, s, l, alpha) {
  * @param {number} l Lightness between 0 and 30.
  * @param {number} a Alpha, 0 or 1
  */
-function twelveBitColor(h, s, l, a = 1) {
+function twelveBitHSLA(h, s, l, a = 1) {
 	let [r, g, b] = hslaToRGBA(h * 4, s / 15, l / 30, 1);
-	r = Math.round(Math.round(r * 15) * 255 / 15);
-	g = Math.round(Math.round(g * 15) * 255 / 15);
-	b = Math.round(Math.round(b * 15) * 255 / 15);
-	return [r, g, b, a * 255];
+	r = Math.round(r * 15);
+	g = Math.round(g * 15)
+	b = Math.round(b * 15);
+	return [r, g, b, a];
+}
+
+function to32BitColor(color) {
+	const r = Math.round(color[0] * 255 / 15);
+	const g = Math.round(color[1] * 255 / 15);
+	const b = Math.round(color[2] * 255 / 15);
+	const a = color[3] * 255
+	return [r, g, b, a];
 }
 
 function meanColor(color1, color2) {
@@ -34,17 +42,11 @@ function meanColor(color1, color2) {
 }
 
 function colorString(color) {
-	return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+	const alpha = color[3] / 255;
+	return `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${alpha})`;
 }
 
 class DitherPattern {
-
-	/**
-	 * @param {string} pattern
-	 */
-	static fromString(pattern, separator = '|') {
-		return new DitherPattern(pattern.split(separator));
-	}
 
 	/**
 	 * @param {string[]} pattern;
@@ -65,6 +67,7 @@ class DitherPattern {
 	 * @param {number[]} color
 	 */
 	async createPattern(context, color) {
+		color = to32BitColor(color);
 		const width = this.width;
 		const height = this.height;
 		const imageData = context.createImageData(width, height);
@@ -73,7 +76,7 @@ class DitherPattern {
 		for (let j = 0; j < height; j++) {
 			const row = this.pattern[j];
 			for (let i = 0; i < width; i++) {
-				if (row[i] === '1') {
+				if (row[i] === 1) {
 					pixels.set(color, offset);
 				}
 				offset += 4;
@@ -86,18 +89,37 @@ class DitherPattern {
 	static checkerboard(modX = 2, emptyRows = 0, right = false) {
 		const numRows = 2 + 2 * emptyRows;
 		const rows = new Array(numRows);
-		rows[0] = '1' + '0'.repeat(modX - 1);
-		const emptyRow = '0'.repeat(modX);
-		rows.fill(emptyRow, 1, 1 + emptyRows);
+
+		const firstRow = new Array(modX);
+		rows[0] = firstRow;
+		firstRow[0] = 1;
+		firstRow.fill(0, 1);
+
+		const emptyRow = new Array(modX);
+		emptyRow.fill(0);
+		for (let i = 1; i <= emptyRows; i++) {
+			rows[i] = emptyRow.slice();
+		}
+
 		const rounding = right ? Math.ceil : Math.trunc;
 		const index = rounding(modX / 2);
-		rows[1 + emptyRows] = rows[0].slice(-index) + rows[0].slice(0, -index);
-		rows.fill(emptyRow, 2 + emptyRows, numRows)
+		const secondRow = firstRow.slice(-index);
+		rows[1 + emptyRows] = secondRow;
+		for (let i = index; i < modX; i++) {
+			secondRow[i] = firstRow[i - index];
+		}
+
+		for (let i = 2 + emptyRows; i < numRows; i++) {
+			rows[i] = emptyRow.slice();
+		}
+
 		return new DitherPattern(rows);
 	}
 
 	static verticalStripes(width = 1, gap = 1) {
-		const row = '1'.repeat(width) + '0'.repeat(gap);
+		const row = new Array(width + gap);
+		row.fill(1, 0, width);
+		row.fill(0, width);
 		return new DitherPattern([row]);
 	}
 }
@@ -105,11 +127,11 @@ class DitherPattern {
 const canvas = document.getElementById('pixel-canvas');
 const context = canvas.getContext('2d');
 
-let fgColor = twelveBitColor(30, 5, 5);
-let bgColor = twelveBitColor(30, 15, 5);
-let fgColorStr = colorString(fgColor);
-let bgColorStr = colorString(bgColor);
-let meanColorStr = meanColor(fgColor, bgColor);
+let fgColor = twelveBitHSLA(30, 5, 5);
+let bgColor = twelveBitHSLA(30, 15, 5);
+let fgColorStr = colorString(to32BitColor(fgColor));
+let bgColorStr = colorString(to32BitColor(bgColor));
+let meanColorStr = meanColor(to32BitColor(fgColor), to32BitColor(bgColor));
 
 let dither = DitherPattern.verticalStripes();
 let pattern = await dither.createPattern(context, fgColor);
